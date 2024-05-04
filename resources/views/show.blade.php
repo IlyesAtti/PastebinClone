9867207b2fd9b5e6c50320b89b341c80e6380c1e
Ilyes Attila <attis.ilyes@gmail.com>
2024-05-04 16:29:48 +0300

    v1.2

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,13 +3,13 @@
 <html>
     <head>
         <title>Post</title>
-        <link rel = "stylesheet" href = "{{url('CSS/custom.css')}}"/>
+        <link rel="stylesheet" href="{{url('CSS/custom.css')}}"/>
     </head>
     
     <body>
         <h1>Post</h1>
         <p>{{ $post->text }}</p>
-        <button onclick = "goBack()" class = "btn btn-primary">Back</button>
+        <button onclick="goBack()" class="btn btn-primary">Back</button>
     </body>
 </html>
 <script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,27 +2,27 @@
 <!DOCTYPE html>
 <html lang = "{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset = "utf-8">
-        <meta name = "viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>PasteBin</title>
 
         <!-- Fonts -->
-        <link rel = "preconnect" href="https://fonts.bunny.net">
-        <link href = "https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel = "stylesheet" />
-        <link rel = "stylesheet" href="{{url('CSS/custom.css')}}"/>
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="{{url('CSS/custom.css')}}"/>
     </head>
 
     <body>
-        <form action = "add" method = "POST">
+        <form action="add" method="POST">
             @csrf <!-- {{ csrf_field() }} -->
-            <textarea rows = "15", cols = "100" id = "id" name = "Text" placeholder = "Enter Your text here" style = "resize:none"></textarea></br>
-            <button type = "submitt">Submit</button>
+            <textarea rows="15", cols="100" id="id" name="Text" placeholder="Enter Your text here" style="resize:none"></textarea></br>
+            <button type="submitt">Submit</button>
         </form>
         
         <table>
             @foreach($post as $text)
                 <tr>
-                    <td><a href = "{{route('post.show', $text->id)}}">{{ $text->teaster }}</a></td>
+                    <td><a href="{{route('post.show', $text->id)}}">{{ $text->teaster }}</a></td>
                 </tr>
             @endforeach
         </table>
